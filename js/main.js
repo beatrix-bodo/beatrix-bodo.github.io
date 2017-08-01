c5b7d0e5cf6fe54057fd57308d9413076203a8b4
Beatrix Bodo <beatrix.bodo@gmail.com>
2017-08-01 14:24:31 +0200

    Add tooltip to copy-to-clipboard, fix some styles, rewrite CSS into SCSS, change responsive breakpoints

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Copy to clipboard
 function copyToClipboard(element) {
   var $temp = $("<input>");
   $("body").append($temp);
@@ -5,3 +6,18 @@ function copyToClipboard(element) {
   document.execCommand("copy");
   $temp.remove();
 }
+
+// Show tooltip after copied to clipboard
+$('.mail-tooltip-icon').click(function(){
+  $( "#copy-mail-tooltip" )
+    .css({ "display": "inline-block" })
+    .delay(500)
+    .fadeOut('fast');
+});
+
+$('.tel-tooltip-icon').click(function(){
+  $( "#copy-tel-tooltip" )
+    .css({ "display": "inline-block" })
+    .delay(500)
+    .fadeOut('fast');
+});
